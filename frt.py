22d8913c046788cd63ffad1e14d0480fe5b10667
VladMalosev <vlad.malosev@gmail.com>
2025-02-23 16:28:27 +0200

    added blink detector using Eye Aspect Ration

--- frt.py
+++ frt.py
@@ -1,44 +1,61 @@
 import cv2
 import dlib
+from scipy.spatial import distance
 
 
-#https://dlib.net/files/
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-tracker = dlib.correlation_tracker()
+# Function to calculate Eye Aspect Ratio (EAR)
+def calculate_ear(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+
+EAR_THRESHOLD = 0.25
+
 detector = dlib.get_frontal_face_detector()
+shape_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-
-
-def detect_eyes_and_eyelids(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = detector(gray)
-
-    if len(faces) > 0:
-        face = faces[0]
-        tracker.start_track(image, face)
-        landmarks = predictor(gray, face)
-        left_eye = [(landmarks.part(i).x, landmarks.part(i).y) for i in range(36, 42)]
-        right_eye = [(landmarks.part(i).x, landmarks.part(i).y) for i in range(42, 48)]
-
-        for (x, y) in left_eye + right_eye:
-            cv2.circle(image, (x, y), 2, (0, 255, 0), -1)
-
-        tracking_quality = tracker.update(image)
-
-        if tracking_quality < 7:
-            tracker.start_track(image, face)
-
-    cv2.imshow("Eye Tracking", image)
-
-# acc webcam
 cap = cv2.VideoCapture(0)
+
+blink_count = 0
+blink_active = False
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    detect_eyes_and_eyelids(frame)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = detector(gray_frame)
+
+    for face in faces:
+        landmarks = shape_predictor(gray_frame, face)
+
+        left_eye = [(landmarks.part(i).x, landmarks.part(i).y) for i in range(36, 42)]
+        right_eye = [(landmarks.part(i).x, landmarks.part(i).y) for i in range(42, 48)]
+
+        left_ear = calculate_ear(left_eye)
+        right_ear = calculate_ear(right_eye)
+        avg_ear = (left_ear + right_ear) / 2.0
+
+        if avg_ear < EAR_THRESHOLD:
+            if not blink_active:
+                blink_active = True
+        else:
+            if blink_active:
+                blink_count += 1
+                blink_active = False
+
+        for point in left_eye + right_eye:
+            cv2.circle(frame, point, 2, (0, 255, 0), -1)
+
+        cv2.putText(frame, f"Left EAR: {left_ear:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+        cv2.putText(frame, f"Right EAR: {right_ear:.2f}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+        cv2.putText(frame, f"Blinks: {blink_count}", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
+    cv2.imshow("Blink Detection", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
